c52b67157898c27514b35f921b94707b6660cdbf
fernandocfbf <fernandocfbf@al.insper.edu.br>
2022-05-24 17:32:04 -0300

    feat: adding double deep q-learning

--- src/utils/PlotRewardsEpisodes.py
+++ src/utils/PlotRewardsEpisodes.py
@@ -1,9 +1,18 @@
 import matplotlib.pyplot as plt
 
 def rewardsEpisodes(rewards):
-    plt.plot(rewards)
+    new_rewards = calculateMetrics(rewards)
+    plt.plot(new_rewards)
     plt.xlabel('Episodes')
-    plt.ylabel('# Rewards')
+    plt.ylabel('# Metrics')
     plt.title('# Rewards vs Episodes')
     plt.savefig("src/results/lunar_lander_v1.jpg")     
     plt.close()
+
+def calculateMetrics(rewards):
+    res = list()
+    for i in range(len(rewards)):
+        if i != 0: 
+            compute = (rewards[i-1] - rewards[1])/(rewards[i-1])
+            res.append(compute)
+    return res
